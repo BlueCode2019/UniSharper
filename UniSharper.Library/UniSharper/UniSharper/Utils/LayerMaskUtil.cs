2369b3952d9a5d2bd02b151133d43812233021b5
Jerry Lee <cosmos53076@163.com>
2017-09-04 13:22:25 +0800

    Add project UniSharper.Extensions.

---
+++ UniSharper.Library/UniSharper/UniSharper/Utils/LayerMaskUtil.cs
@@ -0,0 +1,46 @@
+﻿/*
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2017 Jerry Lee
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+using UnityEngine;
+
+namespace UniSharper.Utils
+{
+    /// <summary>
+    /// This class provides some useful <see cref="LayerMask"/> utilities.
+    /// </summary>
+    public static class LayerMaskUtil
+    {
+        /// <summary>
+        /// Determines whether the specified layer is in the <see cref="LayerMask"/>.
+        /// </summary>
+        /// <param name="layer">The layer.</param>
+        /// <param name="layerMask">The specified <see cref="LayerMask"/>.</param>
+        /// <returns><c>true</c> if the layer is in the <see cref="LayerMask"/>; otherwise, <c>false</c>.</returns>
+        public static bool IsInLayerMask(int layer, LayerMask layerMask)
+        {
+            int objectLayerMaskValue = (1 << layer);
+            return (layerMask.value & objectLayerMaskValue) > 0;
+        }
+    }
+}
