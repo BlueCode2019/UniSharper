e3792840cf87976f3cc6ff80e7458c3c38a87bce
Jerry Lee <cosmos53076@163.com>
2017-09-13 14:32:58 +0800

    Add class UnityHttpClient.

--- UniSharper.Library/UniSharper.Net/Properties/AssemblyInfo.cs
+++ UniSharper.Library/UniSharper.Net/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下 控制。更改这些特性值可修改 与程序集关联的信息。
-[assembly: AssemblyTitle("UniSharper.Extensions")]
+[assembly: AssemblyTitle("UniSharper.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UniSharper.Extensions")]
+[assembly: AssemblyProduct("UniSharper.Net")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 Jerry Lee")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("b8196173-ed10-4930-8036-322d2b291192")]
+[assembly: Guid("94058b39-0296-4c86-95ac-66c1ba045e3f")]
 
 // 程序集的版本信息由下列四个值组成:
 //
